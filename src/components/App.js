a7d5c4976b69aaff9c077bf280f640e817ad8992
OsMax <www.osmax@gmail.com>
2023-07-19 14:12:38 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,6 @@ class App extends Component {
     contacts: initContacts,
     filter: '',
   };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
   onDeleteContact = contactId => {
     this.setState(prevState => ({
